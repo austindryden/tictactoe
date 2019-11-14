8b104db98f5689dd505ca902e1032f8778b9140a
Austin Dryden <austinisnorobot@gmail.com>
2019-11-14 14:31:41 -0500

    fully working

--- tictactoe.py
+++ tictactoe.py
@@ -1,15 +1,14 @@
 def move(board, location, player):
-    x = location[0]
-    y = location[1]
+    x, y = location
     valid = False
     if len(board) != 3 or len(board[0]) != 3:
         print("invalid board, try again")
-        return [['', '', ''], ['', '', ''], ['', '', '']]
-    if not (player == 'X' or player =='Y'):
+        return [['', '', ''], ['', '', ''], ['', '', '']], valid
+    if not (player == 'X' or player =='O'):
         print("invalid player! try again")
     elif (x < 0 or x > 2) or (y < 0 or y > 2):
         print("invalid coordinates! try again!")
-    elif board[x][y] == 'X' or board[x][y] =='Y':
+    elif board[x][y] == 'X' or board[x][y] =='O':
         print("somebody already moved there! try again!")
     else:
         board[x][y] = player
@@ -38,41 +37,39 @@ def tictactoe():
     play = True
     while play:
         x_win = False
-        y_win = False
+        o_win = False
         moves = 0
         turn = "X"
         board = [[' ', ' ', ' '], [' ', ' ', ' '], [' ', ' ', ' ']]
         print("LETS PLAY TIC TAC TOE!!!!")
-        while not x_win and not y_win and moves < 9:
+                
+        while not x_win and not o_win and moves < 9:
             printboard(board)
-            print(f"player {turn}, its your turn! where do you wanna go?")
+            print(f"player {turn}, its your turn! where do you wanna go? ")
             valid = False
             
             while not valid:
-                x = int(input("what row would you like to move?")) - 1
-                y = int(input("what column would you like to move?")) - 1 
+                x = int(input("what row would you like to move? ")) - 1
+                y = int(input("what column would you like to move? ")) - 1 
                 board, valid = move(board,(x,y), turn)
-                
-            
-            printboard(board)
-            
+                           
             if turn == "X":
-                turn = "Y"
+                turn = "O"
             else:
                 turn = "X"
+            
             x_win = tictactoewin(board, "X")
-            y_win = tictactoewin(board, "Y")
-            print(x_win)
-            print(y_win)
-            moves +=1
+            o_win = tictactoewin(board, "O")
+            moves += 1
+        
+        printboard(board)
         if x_win:
             print("X wins! good job!")
-        elif y_win:
-            print("Y wins! good job!")
+        elif o_win:
+            print("O wins! good job!")
         else:
             print("Its a tie?!?")
         play = input("would you like to play again? ('YES' to continue)") == "YES"
     print("Thanks for playing!")
-
 
 tictactoe()
